3a109726e385c97cc97bb87c650aa964e8f853ab
Rosset Ignacio <rossetquitomolina@gmail.com>
2018-05-05 00:06:23 -0500

    cambios_mantenimientos

--- views/mantenimiento/chofer_mantenimiento_nuevo.php
+++ views/mantenimiento/chofer_mantenimiento_nuevo.php
@@ -1,5 +1,5 @@
 <?php
-require ROOT_PATH . "views/shared/content-float.php";
+require ROOT_PATH . "views/shared/content-float-modal.php";
 ?>	
 <?php
 
@@ -8,7 +8,7 @@ function fncTitle() { ?>Nuevo Chofer<?php } ?>
 <?php
 
 function fncHead() { ?>
-
+<script type="text/javascript" src="include/js/jForm.js"></script>
     
 <?php } ?>
 
@@ -26,19 +26,29 @@ function fncMenu() { ?>
 function fncPage() { ?>
 <?php if (!isset($GLOBALS['resultado']) || $GLOBALS['resultado'] == -1) { ?>
 <form id="form"  method="POST" action="/Mantenimiento/Chofer_Mantenimiento_Nuevo" style="width:600px; padding-top:15px;" onsubmit="return validar();" >
-    <div class="row">
+    <div class="panel panel-tab rounded shadow">
+        <div class="panel-body no-padding rounded-bottom">
+            <div class="tab-content form-horizontal">
+                <div id="divDatos" class="tab-pane fade in active inner-all">
+    <div class="form-group">
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
             <label>Persona: </label>&nbsp;&nbsp;&nbsp;<a class="btn btn-success" title="Agregar persona nueva" onclick="fncAgregar_Persona();"><img src="/include/img/boton/add_user-20.png"></a>
         </div>
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-            <input type="text" id="txtPersona_ID" class="form-control form-requerido">
+<!--            <input type="text" id="txtPersona_ID" class="form-control form-requerido">
             <script type="text/javascript">
                 var cboPersona = newCbo('divPersona', 'txtPersona_ID', '/funcion/ajaxCbo_Persona', true);
-            </script>
+            </script>-->
+                                        <select id="selPersona" name="selPersona" class="chosen-select">
+                               <option value="0">--Seleccionar--</option>
+                                <?php foreach($GLOBALS['dtPersona'] as $persona){?>
+                               <option value="<?php echo $persona['ID']?>"><?php echo FormatTextView(strtoupper($persona['apellido_paterno']. ' '. $persona['apellido_materno']. ' ' . $persona['nombres']));?></option>
+                                <?php }?>
+                            </select>
         </div>
 
     </div>
-    <div class="row">
+    <div class="form-group">
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
             <label>Licencia conducir: </label>
         </div>
@@ -46,7 +56,7 @@ function fncPage() { ?>
             <input type="text" id="txtLicencia_Conducir" name="txtLicencia_Conducir" value="<?php echo $GLOBALS['oChofer']->licencia_conducir;?>" autocomplete="off" class="form-control form-requerido text-uppercase">
         </div>
     </div>
-    <div class="row">
+    <div class="form-group">
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
             <label>Celular: </label>
         </div>
@@ -54,7 +64,7 @@ function fncPage() { ?>
             <input type="text" id="txtCelular" name="txtCelular" value="<?php echo $GLOBALS['oChofer']->celular;?>" autocomplete="off" class="form-control bfh-number int">
         </div>
     </div>
-    <div class="row">
+    <div class="form-group">
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
             <label>Estado: </label>
         </div>
@@ -67,16 +77,22 @@ function fncPage() { ?>
 
         </div>
     </div>
+        </div>
+            </div>
+        </div>
+        <div class="panel-footer">
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <button  id="btnEnviar" name="btnEnviar" class="btn btn-success" >
-                <img title="Guardar" alt="" src="/include/img/boton/save_14x14.png">
+            <button  id="btnEnviar" name="btnEnviar" class="btn btn-success" title="Guardar">
+                <img alt="" width="16" src="/include/img/boton/save_48x48.png">
                 Guardar
             </button>
-            <button  id="btnCancelar" name="btnCancelar" class="btn btn-danger" type="button" onclick="window_float_close();" >
-                <img title="Guardar" alt="" src="/include/img/boton/cancel_14x14.png">
+            <button  id="btnCancelar" name="btnCancelar"  class="btn btn-danger" type="button" title="Salir" onclick="salir();" >
+                <img  alt="" src="/include/img/boton/cancel_14x14.png">
                 Cancelar
-            </button>   
+            </button>  
+        </div>
+    </div>
         </div>
     </div>
 </form>
@@ -96,10 +112,22 @@ function fncPage() { ?>
         }
     }
     var fncAgregar_Persona=function(){
-        window_float_deslizar('form','/Mantenimiento/Persona_Mantenimiento_Nuevo','','');
+//       window_float_deslizar('form','/Mantenimiento/Persona_Mantenimiento_Nuevo','','');
+         parent.window_float_open_modal_hijo("AGREGAR NUEVA PERSONA",'Mantenimiento/Persona_Mantenimiento_Nuevo','','',fncCargarPersona,850,500);
     } 
-    var fncCargarPersona=function(id,nombres){
-        cboPersona.seleccionar(id, nombres);
+    var fncCargarPersona=function(id){
+        cargarValores("/funcion/ajaxOption_Persona",id,function(resultado){
+            $('#selPersona').html(resultado.html);
+            $('#selPersona').val(id);
+            $('#selPersona').trigger("chosen:updated");
+            
+        });
+//        cboPersona.seleccionar(id, nombres);
+//       alert(nombres);
+    }
+            var salir=function(){
+        //window.parent.fncMantenimiento();
+        window_float_close_modal();
     }
     
 </script>
@@ -108,7 +136,7 @@ function fncPage() { ?>
     <script type="text/javascript">
         $(document).ready(function(){
             toastem.success("<?php echo $GLOBALS['mensaje']; ?>");
-            setTimeout('window_float_save();', 1000);
+            setTimeout('window_float_save_modal();', 1000);
         });       
        
        
